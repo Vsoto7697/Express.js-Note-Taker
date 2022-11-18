2cb02abc3df421db61750ee432acf40dbf9e792c
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-11-17 23:04:27 -0500

    insantiate the sever and told the server to listen for requests

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+// instantiate the server
+const app = express();
+// tell server to listen for requests
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
+
+  
