9d826d468f06a03babf40fd8408024209a889caf
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-11-17 23:40:14 -0500

    handled requests for notes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//handle requests for notes
 app.get('/api/notes', (req, res) => {
     res.json(theNotes.slice(1));
 });
