57eb0ed7a0b5ecdee0341e9461fc3b58640af29d
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-11-18 01:56:31 -0500

    edited html files to properly display correctly

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 
 // Set PORT
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
